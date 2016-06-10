ee0eb3b5144e6dead2e8f02b4323acb2ea1bfd02
Annshuk <annshuks@gmail.com>
2016-06-10 14:11:32 +0530

    Signed-off-by: Annshuk <annshuks@gmail.com>

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
 // JavaScript Document
 // http://json-generator.appspot.com/api/json/get/cavnOIbUoi?indent=2  
-/*$.getJSON(ImageUrl, function(data, status){
-			  $.each(data, function(i, obj){
-				  $.each(obj, function(i, value){					  
-					$("#generateTiles").append("<li><a href='javascript:;'><img src="+value.image+" data-name='"+value.phrase+"'></a></li>");	
-				});
-			});
-		})*/
-var ImageUrl = "http://json-generator.appspot.com/api/json/get/cmrArhUQMO?indent=2";		
-$(function(){	
 
-   $.ajax({
-       url: ImageUrl, 	      
-       dataType: "json",
-	   success:getImageforTiles
-    });
-});
+$(function(){
+var images=[], imgPhrase = [], openStatus = false, ImageUrl = "http://json-generator.appspot.com/api/json/get/cmrArhUQMO?indent=2";	
 
-//generate random Number between 6 to 12
-function randomIntNumber(min,max){
+var randomIntNumber = function(min,max){
     return Math.floor(Math.random()*(max-min+1) + min);
 }
 //created Factroy Pattern
 var getJsonData = {
-		read:  function(){
+		read: function(){
 			return $.getJSON(ImageUrl).then(function(response){
 				return response.data;							
-			})	
+			});
 		}
-	}
+}
+// New Board
+var newBoardGame = function(){
+	var opaque = randomIntNumber(6,9),
+		duplicateOpaque = opaque * 2;  
+		  getJsonData.read().done(function(data){
+			  for(k in data){
+				  images.push(data[k]["image"]);
+				  imgPhrase.push(data[k]["phrase"]);
+					 
+			  }
+			  for(var i=0; i< duplicateOpaque; i++){	
+					var randomImg = Math.floor( Math.random() * images.length);
+				    $("#generateTiles").append("<li data-id='image"+i+"'>\
+						  <div class='insdie'>\
+						  <div class='front'><img src='"+images[randomImg]+"' data-name='"+imgPhrase[randomImg]+"'></div>\
+						  <div class='back'></div>\
+						  </div></li>"
+				    );
+				}			 	 
+	  });  		
+		
+}
+newBoardGame()
+});
 
 
-var getImageforTiles = function(){	
-		var opaque = randomIntNumber(6,9) * 2;
-           getJsonData.read().done(function(data,status){
-		   for( k in data){
-			$("#generateTiles").append("<li><a href='javascript:;'><img src='"+data[k]['image']+"' data-name='"+data[k]['phrase']+"'></a></li>")
-			}
-		  //for(var i=1; i<= opaque; i++){			 	  
-			 //$("#generateTiles").append("<li><a href='javascript:;'><img src='"+data[k]['image']+"' data-name='"+data[k]['phrase']+"'></a></li>");
-	 //  }		
-	  }) 
-	  	
-	   
-    }
+//generate random Number between 6 to 12
+
+
+//random Images
+var randomImage = function(){
+	Array.prototype.RandomImage = function(){
+		var len = this.length, j, temp;
+		while(--len){
+			j = Math.floor( Math.random() * (len-1));
+			temp = this[len];
+			this[len] = this[j];
+			this[j] = temp;	
+		}	
+	};
+	images.RandomImage();
+	imgPhrase.RandomImage();
+}
 
 
